14e58831be427db685c77039ec6cf01d9b596dcd
LouisZ-88 <s5530609@gmail.com>
2026-01-09 11:04:53 +0800

    變更code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./{App,index}.{js,ts,jsx,tsx}",
+    "./{components,services}/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
